5ca64db51e27cdc17b5f716219f7e70cee820d1b
Brandon Ng <446038117@qq.com>
2023-06-13 22:05:04 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -2,7 +2,7 @@ const BLOG = {
   title: 'Brandon Space',
   author: 'Brandon Ng',
   email: 'brandonng@foxmail.com',
-  link: 'https://nobelium.vercel.app',
+  link: 'https://brandonng.cc',
   description: 'This gonna be an awesome place to say something.',
   lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   timezone: 'Asia/Shanghai', // Your Notion posts' date will be interpreted as this timezone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
