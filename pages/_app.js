fc8d5ef2d715368b95e180ab57df1a9c18bd0b01
Brandon Ng <446038117@qq.com>
2023-06-27 03:38:17 +0800

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -12,9 +12,6 @@ import { prepareDayjs } from '@/lib/dayjs'
 import { ThemeProvider } from '@/lib/theme'
 import Scripts from '@/components/Scripts'
 
-const Ackee = dynamic(() => import('@/components/Ackee'), { ssr: false })
-const Gtag = dynamic(() => import('@/components/Gtag'), { ssr: false })
-
 import Script from 'next/script'
 export default function MyApp({ Component, pageProps }) {
   return (
