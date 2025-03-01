613cc2b38c5d0a4b653f8cee56b85f7ccbc3b11b
Brandon Ng <446038117@qq.com>
2025-03-01 13:51:25 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -2,7 +2,7 @@ const BLOG = {
   title: 'Helios',
   author: 'Brandon Ng',
   email: 'brandonng0824@gmail.com',
-  link: 'https://brandonng.cc',
+  link: 'https://brandonng.cc/about',
   description: 'This gonna be an awesome place to say something.',
   lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   timezone: 'Asia/Shanghai', // Your Notion posts' date will be interpreted as this timezone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
@@ -18,7 +18,7 @@ const BLOG = {
   showArchive: true,
   autoCollapsedNavBar: false, // The automatically collapsed navigation bar
   ogImageGenerateURL: 'https://og-image-craigary.vercel.app', // The link to generate OG image, don't end with a slash
-  socialLink: 'mailto:brandonng@foxmail.com',
+  socialLink: 'mailto:brandonng0824@gmail.com',
   seo: {
     keywords: ['Blog', 'Notion'],
     googleSiteVerification: '' // Remove the value or replace it with your own google site verification code
